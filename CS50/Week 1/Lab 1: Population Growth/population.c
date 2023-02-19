4844422ffea2c9af00a6601c8a721fc672b9f0c6
jmr551 <joserivas12345@gmail.com>
2023-02-19 08:25:05 +0000

    Sun, Feb 19, 2023, 9:25 AM +01:00

--- CS50/Week 1/Lab 1: Population Growth/population.c
+++ CS50/Week 1/Lab 1: Population Growth/population.c
@@ -3,11 +3,21 @@
 
 int main(void)
 {
+    int n, end_size, c = 0;
+
     // TODO: Prompt for start size
+    n = get_int("Start size: ");
 
     // TODO: Prompt for end size
+    end_size = get_int("End size: ");
 
     // TODO: Calculate number of years until we reach threshold
+    while (n < end_size)
+    {
+        n = n + n/3 - n/4;
+        c++;
+    }
 
     // TODO: Print number of years
+    printf("Years: %d\n",c);
 }
