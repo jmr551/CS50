67ff807fd863da30436a41cfe9cebde54cd927a9
jmr551 <joserivas12345@gmail.com>
2023-02-19 17:08:47 +0000

    Sun, Feb 19, 2023, 6:08 PM +01:00

---
+++ CS50/Week 1/ps1/mario-more/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_height();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j=)
+    }
+}
+
+int get_height()
+{
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1 || n > 8);
+
+    return n;
+}
