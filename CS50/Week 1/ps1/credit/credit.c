c9d690a6076b80be23e5806e61a702eed83bdd5f
jmr551 <joserivas12345@gmail.com>
2023-02-20 11:20:15 +0000

    Mon, Feb 20, 2023, 12:20 PM +01:00

--- CS50/Week 1/ps1/credit/credit.c
+++ CS50/Week 1/ps1/credit/credit.c
@@ -5,13 +5,16 @@ int main(void)
 {
     int pares = 0, impares = 0;
     long card = get_long();
+    long card_copia = card;
     bool visa = false, master = false, checsum = false;
 
     // Verificamos primero el checksum
     for (int i = 0; i < 16; i++)
     {
-        
-
+        if (i % 2 == 0) impares += card_copia % 10;
+        else pares += 2* card_copia % 10;
+        card_copia/=10;
     }
-
+    checksum = ((pares + impares) % 10 == 0);
+    printf(checksum);
 }
