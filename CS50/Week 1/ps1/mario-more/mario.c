47255f0a3a395d550ff2c8cf93d6853caca1099f
jmr551 <joserivas12345@gmail.com>
2023-02-20 10:21:37 +0000

    Mon, Feb 20, 2023, 11:21 AM +01:00

--- CS50/Week 1/ps1/mario-more/mario.c
+++ CS50/Week 1/ps1/mario-more/mario.c
@@ -10,6 +10,15 @@ int main(void)
         // This is for the first part
         for (int j = 0; j<n; j++)
         {
+            // ___# n=4, i=[0,3], espacios = []
+            if ()
+            {
+
+            }
+            else
+            {
+
+            }
 
         }
     }
