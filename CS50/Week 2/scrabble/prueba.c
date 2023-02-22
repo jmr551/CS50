3f416251e8265c72a5dd981b33c02d84e94f5487
jmr551 <joserivas12345@gmail.com>
2023-02-22 19:59:05 +0000

    Wed, Feb 22, 2023, 8:59 PM +01:00

---
+++ CS50/Week 2/scrabble/prueba.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    string var;
+    var = "Hola";
+    printf("%s", var);
+
+    /*for (int i = 0; i < strlen(var); i++)
+    {
+        printf("%c", toupper(var[i]));
+    }*/
+    printf("\n");
+}
