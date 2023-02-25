9c8cc4719f8382979d10e231333b161947cf8673
jmr551 <joserivas12345@gmail.com>
2023-02-25 14:21:11 +0000

    Sat, Feb 25, 2023, 3:21 PM +01:00

--- CS50/Week 2/no-vowels/no-vowels.c
+++ CS50/Week 2/no-vowels/no-vowels.c
@@ -6,7 +6,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void replace(string )
+string replace(string word);
 
 int main(int argc, string argv[])
 {
@@ -15,7 +15,21 @@ int main(int argc, string argv[])
         printf("Usage: ./no-vowels.c word\n");
         return 1;
     }
+    string converted = replace(argv[1]);
 
+    printf("%s\n", converted);
 
+    return 0;
+}
 
+string replace(string word)
+{
+    string hola;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+    }
+
+    return
 }
