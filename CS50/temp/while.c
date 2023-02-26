b0714a209614b5b7b7138d0d6c688302f35bc4d2
jmr551 <joserivas12345@gmail.com>
2023-02-26 17:06:52 +0000

    Sun, Feb 26, 2023, 6:06 PM +01:00

---
+++ CS50/temp/while.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float n;
+    
+
+    if () //Si n está entre 100 y 200
+    {
+
+    }
+    else //Si no está entre 100 y 200
+    {
+
+    }
+}
