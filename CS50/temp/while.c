6cdb312164a2cceeb663b398a0f653e4929a8581
jmr551 <joserivas12345@gmail.com>
2023-02-26 17:10:51 +0000

    Sun, Feb 26, 2023, 6:10 PM +01:00

--- CS50/temp/while.c
+++ CS50/temp/while.c
@@ -1,17 +1,20 @@
-#include <iostream>
+#include <iostream.h>
 using namespace std;
 
+
+// Que el usuario ingrese un numero entre 100 y 200. Si el numero es valido, imprimir el doble.
+// Imprimir un error en caso contrario.
 int main()
 {
     float n;
-    
+    cin << n;
 
-    if () //Si n está entre 100 y 200
+    if (n>=100 and n<=200) //Si n está entre 100 y 200
     {
-
+        cout<<2*n;
     }
     else //Si no está entre 100 y 200
     {
-
+        cout<<"El numero no esta entre 100 y 200.";
     }
 }
