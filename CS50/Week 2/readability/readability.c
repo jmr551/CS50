0cbf5fdfba9128787bc094fc4f3ada80316caba3
jmr551 <joserivas12345@gmail.com>
2023-02-27 11:49:08 +0000

    Mon, Feb 27, 2023, 12:49 PM +01:00

--- CS50/Week 2/readability/readability.c
+++ CS50/Week 2/readability/readability.c
@@ -4,13 +4,15 @@
 
 int count_letters(string s);
 int count_words(string s);
+
+
 int main(void)
 {
     string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
-    printf("%i letters\", letters);
-    printf("%i words", words);
+    printf("%i letters\n", letters);
+    printf("%i words\n", words);
 }
 
 int count_letters(string s)
