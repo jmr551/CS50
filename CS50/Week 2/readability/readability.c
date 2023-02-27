8ccd195250dd24689b06243ac8cf745e994e8482
jmr551 <joserivas12345@gmail.com>
2023-02-27 11:33:25 +0000

    Mon, Feb 27, 2023, 12:33 PM +01:00

--- CS50/Week 2/readability/readability.c
+++ CS50/Week 2/readability/readability.c
@@ -1,10 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string s);
+
 int main(void)
 {
     int words=0,
     string text = get_string("Text: ");
 
 
+}
+
+int count_letters(string s)
+{
+    
 }
