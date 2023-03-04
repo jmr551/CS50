fb9a3ccb9258853ab783adb83e04a26903becac1
jmr551 <joserivas12345@gmail.com>
2023-03-04 14:00:53 +0000

    Sat, Mar 4, 2023, 3:00 PM +01:00

--- CS50/Week 2/substitution/substitution.c
+++ CS50/Week 2/substitution/substitution.c
@@ -3,12 +3,13 @@
 
 int main(int argc, string argv[])
 {
-    if ()
+    if (argc == 2)
     {
         
     }
     else
     {
-
+        printf ("Error. You must introduce one key and just one.");
+        return 1;
     }
 }
