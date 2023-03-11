79eeab057d73af8c912e831290754fb782b7af42
jmr551 <joserivas12345@gmail.com>
2023-03-11 21:41:22 +0000

    Sat, Mar 11, 2023, 10:41 PM +01:00

--- CS50/Week 3/plurality/plurality.c
+++ CS50/Week 3/plurality/plurality.c
@@ -80,7 +80,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int mayor = candidates[0].votes, cant_mayores = 0;
+    int mayor = candidates[0].votes;
 
     for (int i = 1; i < candidate_count; i++)
     {
@@ -92,9 +92,9 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidate[i].votes == mayor)
+        if (candidates[i].votes == mayor)
         {
-            printf ("%s\n",candidate[i].name);
+            printf ("%s\n",candidates[i].name);
         }
     }
 
