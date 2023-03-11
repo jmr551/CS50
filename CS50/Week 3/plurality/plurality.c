9ca705471f03961121234423aaee049807da1f7c
jmr551 <joserivas12345@gmail.com>
2023-03-11 21:29:47 +0000

    Sat, Mar 11, 2023, 10:29 PM +01:00

--- CS50/Week 3/plurality/plurality.c
+++ CS50/Week 3/plurality/plurality.c
@@ -80,6 +80,10 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+    int mayor = candidates[0].votes;
+    for (int i = 1; i < candidate_count; i++)
+    {
+        
+    }
     return;
 }
