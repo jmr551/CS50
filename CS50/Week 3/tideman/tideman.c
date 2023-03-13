af20d369be40bc44cdd44d08087c9b88650e76ef
jmr551 <joserivas12345@gmail.com>
2023-03-13 17:20:22 +0000

    Mon, Mar 13, 2023, 6:20 PM +01:00

--- CS50/Week 3/tideman/tideman.c
+++ CS50/Week 3/tideman/tideman.c
@@ -127,7 +127,9 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    pairs[]
+    pairs[].winner =
+    pairs[].loser =
+    pair_count++;
     return;
 }
 
