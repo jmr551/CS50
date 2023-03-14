2d78c3f4f0bf7dd74ac043b38ae663998445d361
jmr551 <joserivas12345@gmail.com>
2023-03-14 11:13:01 +0000

    Tue, Mar 14, 2023, 12:13 PM +01:00

--- CS50/Week 3/tideman/tideman.c
+++ CS50/Week 3/tideman/tideman.c
@@ -182,7 +182,7 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    (i > pair_count)// TODO
     return;
 }
 
