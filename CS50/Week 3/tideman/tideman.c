c0a9340f1b4c5a259fbd30e2cc005026fc1c49df
jmr551 <joserivas12345@gmail.com>
2023-03-14 12:48:49 +0000

    Tue, Mar 14, 2023, 1:48 PM +01:00

--- CS50/Week 3/tideman/tideman.c
+++ CS50/Week 3/tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -129,7 +130,7 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    for (int = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count - 1; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
@@ -237,7 +238,7 @@ bool ciclo(int len, int v_orig[], int pos) //prueba el recibido y a cada uno de 
             if (locked[v_orig[pos]][i] && pos + 1 < len)
             {
                 v_orig[pos+1] = i;
-                ciclo(len, v_orig, pos + 1)
+                ciclo(len, v_orig, pos + 1);
             }
         }
     }
