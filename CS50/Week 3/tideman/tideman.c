fab3645d7989f77379e69eb0078bac8a6e0e618e
jmr551 <joserivas12345@gmail.com>
2023-03-14 13:33:58 +0000

    Tue, Mar 14, 2023, 2:33 PM +01:00

--- CS50/Week 3/tideman/tideman.c
+++ CS50/Week 3/tideman/tideman.c
@@ -117,11 +117,11 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count - 1; i++) //i representa el rank ganador
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++) // j representa a los que pierden contra i
         {
-            preferences[i][j] += 1;
+            preferences[ranks[i]][ranks[j]] += 1;
         }
     }
     return;
