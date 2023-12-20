9255da0fd66eaa5f84b0bde287adddd8ffe629fd
jmr551 <joserivas12345@gmail.com>
2023-12-20 14:34:08 +0000

    Wed, Dec 20, 2023, 3:34 PM +01:00

--- CS50/Week 5/inheritance/inheritance.c
+++ CS50/Week 5/inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    ()malloc
+    person* p= (person*) malloc (sizeof(person))
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -49,7 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-
+        p->parent0 = parent0;
+        p->parent1 = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
