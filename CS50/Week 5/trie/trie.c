2cc2170ec55e990ffee2ddbeb135bf8665684be0
jmr551 <joserivas12345@gmail.com>
2023-12-26 20:43:33 +0000

    Tue, Dec 26, 2023, 9:43 PM +01:00

--- CS50/Week 5/trie/trie.c
+++ CS50/Week 5/trie/trie.c
@@ -109,6 +109,11 @@ int main(int argc, char *argv[])
 // TODO: Complete the check function, return true if found, false if not found
 bool check(char *word)
 {
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+    }
+
     return false;
 }
 
