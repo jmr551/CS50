f14198f54ff39b4c5bc31fd20e8be92ba0a65767
jmr551 <joserivas12345@gmail.com>
2023-12-30 21:24:29 +0000

    Sat, Dec 30, 2023, 10:24 PM +01:00

--- CS50/Week 5/speller/dictionary.c
+++ CS50/Week 5/speller/dictionary.c
@@ -57,7 +57,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
+    FILE *dict = fopen(text, "r");
     return false;
 }
 
