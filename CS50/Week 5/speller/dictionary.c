e36fc6feb8df5627b7ec3a72a2e78621de51e610
jmr551 <joserivas12345@gmail.com>
2023-12-30 19:31:56 +0000

    Sat, Dec 30, 2023, 8:31 PM +01:00

--- CS50/Week 5/speller/dictionary.c
+++ CS50/Week 5/speller/dictionary.c
@@ -12,13 +12,6 @@ typedef struct node
     struct node *next;
 } node;
 
-/*typedef struct node
-{
-    bool is_word;
-    struct node *children[SIZE_OF_ALPHABET];
-} node;
-*/
-
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 27*27*27*27; // Elijo hacerlo 4 veces (incluyo el espacio)
 
