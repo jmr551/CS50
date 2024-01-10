d87144c8f485f9b8fbeca8133690c4d368dd6f69
jmr551 <joserivas12345@gmail.com>
2024-01-10 20:59:57 +0000

    Wed, Jan 10, 2024, 9:59 PM +01:00

--- CS50/Week 4/volume/volume.c
+++ CS50/Week 4/volume/volume.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     fwrite(h,sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    char a[1];
+    int16_t buffer;
     while (input != NULL)
     {
         fgets(a, 1, input);
