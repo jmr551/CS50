e0b9ce78fe615c0ac0aa7bc21b6d58a6fbfa0bb2
jmr551 <joserivas12345@gmail.com>
2024-01-10 21:01:20 +0000

    Wed, Jan 10, 2024, 10:01 PM +01:00

--- CS50/Week 4/volume/volume.c
+++ CS50/Week 4/volume/volume.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[])
     int16_t buffer;
     while (input != NULL)
     {
+        fread(&buffer, sizeof(int16_t), 1, input)
         fgets(a, 1, input);
         fwrite(output, )
     }
