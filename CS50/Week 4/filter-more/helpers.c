8e101711f1fcc5c146df563d573f1c4af619d6e5
jmr551 <joserivas12345@gmail.com>
2024-01-11 17:46:26 +0000

    Thu, Jan 11, 2024, 6:46 PM +01:00

--- CS50/Week 4/filter-more/helpers.c
+++ CS50/Week 4/filter-more/helpers.c
@@ -5,13 +5,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
+            printf("SE LE LLAMA A ESTA FUNCION?\n");
+
         for (int j = 0; j < width; j++)
         {
             //BYTE prom = BYTE(round((float(image[i][j]->rgbtBlue) + float(image[i][j]->rgbtGreen) + float(image[i][j]->rgbtRed))/3));
             // BYTE prom = WORD(DWORD(image[i][j].rgbtBlue) + DWORD(image[i][j].rgbtGreen) + DWORD(image[i][j].rgbtRed))/3;
             if (i == 0 && j == 0)
             {
-                printf("numero: %d", image[i][j].rgbtBlue);
+                printf("numero: %d", int(image[i][j].rgbtBlue));
             }
         }
     }
