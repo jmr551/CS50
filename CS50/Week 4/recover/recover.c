2606f8fac8762bef8d76ad9fb3d60df6c9e441ab
jmr551 <joserivas12345@gmail.com>
2024-01-12 02:46:14 +0000

    Fri, Jan 12, 2024, 3:46 AM +01:00

--- CS50/Week 4/recover/recover.c
+++ CS50/Week 4/recover/recover.c
@@ -24,6 +24,9 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0xe0)
         {
             printf("Encontramos la foto %d\n", cont+1);
+            // Grabamos la foto...
+            
+
             cont++;
         }
     }
