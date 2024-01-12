440c3d90f4f295883c071f4ca859d9c384f565b1
jmr551 <joserivas12345@gmail.com>
2024-01-12 02:55:34 +0000

    Fri, Jan 12, 2024, 3:55 AM +01:00

--- CS50/Week 4/recover/recover.c
+++ CS50/Week 4/recover/recover.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
 
     int cont = 0;
-    char file_name[];
+    char file_name[8];
     while (fread(buffer, 1, 512, file))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0xe0)
         {
-            printf("Encontramos la foto %d\n", cont+1);
+            //printf("Encontramos la foto %d\n", cont+1);
             // Grabamos la foto...
-            sprintf(file_name, "%d",cont);
+            sprintf(file_name, "%d.jpg",cont);
             printf("%s\n", file_name);
             cont++;
         }
