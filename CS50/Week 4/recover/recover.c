197b865d96724000ebd8015df3aefd0991cd3218
jmr551 <joserivas12345@gmail.com>
2024-01-12 03:16:50 +0000

    Fri, Jan 12, 2024, 4:16 AM +01:00

--- CS50/Week 4/recover/recover.c
+++ CS50/Week 4/recover/recover.c
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0xe0)
         {
-            
             sprintf(file_name, "%03d.jpg",cont);
+            FILE *nueva_foto = fopen(file_name, "w");
             cont++;
         }
     }
