0fb6027f178fd61fe614dbfe0119d33c63691865
jmr551 <joserivas12345@gmail.com>
2024-01-12 03:19:56 +0000

    Fri, Jan 12, 2024, 4:19 AM +01:00

--- CS50/Week 4/recover/recover.c
+++ CS50/Week 4/recover/recover.c
@@ -20,13 +20,20 @@ int main(int argc, char *argv[])
 
     int cont = 0;
     char file_name[8];
+    bool abierto = false;
     while (fread(buffer, 1, 512, file))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)==0xe0)
         {
             sprintf(file_name, "%03d.jpg",cont);
             FILE *nueva_foto = fopen(file_name, "w");
+            abierto = true;
+            fwrite(buffer, 1, 512, nueva_foto);
             cont++;
+        }
+        else if(abierto)
+        {
+            
         }
     }
 
