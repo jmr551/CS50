02f4a3030345bd5c753080f4ac65220dbedde00a
jmr551 <joserivas12345@gmail.com>
2024-01-22 22:20:12 +0000

    Mon, Jan 22, 2024, 11:20 PM +01:00

--- CS50/Week 7/fiftyville/log.sql
+++ CS50/Week 7/fiftyville/log.sql
@@ -48,3 +48,4 @@ WHERE day = 28 AND month = 7;
 -- As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
 -- In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
 -- The thief then asked the person on the other end of the phone to purchase the flight ticket.
+
