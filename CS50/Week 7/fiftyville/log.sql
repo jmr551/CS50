cd944e194ec0e8e17796fb2a16e66b8631291b41
jmr551 <joserivas12345@gmail.com>
2024-01-22 21:50:46 +0000

    Mon, Jan 22, 2024, 10:50 PM +01:00

--- CS50/Week 7/fiftyville/log.sql
+++ CS50/Week 7/fiftyville/log.sql
@@ -22,6 +22,6 @@ WHERE month = 7 AND day = 28;
 
 -- So I need to see who entered before 10:15 and has gone after that
 
-(SELECT activity, license_plate, hour, minute, month, year
+SELECT activity, license_plate, hour, minute, month, year
 FROM bakery_security_logs
-WHERE month = 7 AND day = 28)
+WHERE month = 7 AND day = 28
