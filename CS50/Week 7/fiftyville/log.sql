e4ca1c45b7bbda849e35fe117be535c80df8d0db
jmr551 <joserivas12345@gmail.com>
2024-01-22 21:32:45 +0000

    Mon, Jan 22, 2024, 10:32 PM +01:00

--- CS50/Week 7/fiftyville/log.sql
+++ CS50/Week 7/fiftyville/log.sql
@@ -1,3 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 .tables -- Because I would like to know what tables are in the database
 .schema crime_scene_reports
+
+-- I want to know the crime scene reports from that day
+SELECT description
+FROM crime_scene_reports
+WHERE month = 7 AND day = 28
+AND street = 'Humphrey Street';
+
+
