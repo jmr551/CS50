a627135fc7589cfa5c08d72c3ab056708be43d48
jmr551 <joserivas12345@gmail.com>
2024-01-22 19:26:35 +0000

    Mon, Jan 22, 2024, 8:26 PM +01:00

--- CS50/Week 7/movies/12.sql
+++ CS50/Week 7/movies/12.sql
@@ -4,7 +4,7 @@ WHERE id IN (
     SELECT movie_id
     FROM stars
     WHERE person_id IN (
-        
+
     )
 )
 
@@ -13,6 +13,11 @@ SELECT id
 FROM people
 WHERE name = 'Bradley Cooper'
 
-SELECT id
-FROM people
-WHERE name = 'Jennifer Lawrence'
+SELECT movie_id
+FROM stars
+WHERE person_id = (
+    SELECT id
+    FROM people
+    WHERE name = 'Jennifer Lawrence'
+)
+
