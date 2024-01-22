338a0dbebba3c3ca5cae5c8b936432f51f94756e
jmr551 <joserivas12345@gmail.com>
2024-01-22 22:22:21 +0000

    Mon, Jan 22, 2024, 11:22 PM +01:00

--- CS50/Week 7/fiftyville/log.sql
+++ CS50/Week 7/fiftyville/log.sql
@@ -23,7 +23,7 @@ WHERE month = 7 AND day = 28;
 -- So I need to see who entered before 10:15 and has gone after that
 
 -- My list of possible thiefs is
-SELECT license_plate-- activity, license_plate, hour, minute, month, year
+SELECT activity, license_plate, hour, minute, month, year
 FROM bakery_security_logs
 WHERE month = 7 AND day = 28
 AND license_plate NOT IN (
