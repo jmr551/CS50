63ffcb19350650a4efce1df23d0a63177d1e4c1b
jmr551 <joserivas12345@gmail.com>
2024-01-22 19:10:53 +0000

    Mon, Jan 22, 2024, 8:10 PM +01:00

--- CS50/Week 7/movies/11.sql
+++ CS50/Week 7/movies/11.sql
@@ -3,7 +3,9 @@ FROM movies
 WHERE id IN (
     SELECT movie_id
     FROM stars
-    WHERE person_id IN (
-        
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Chadwick Boseman'
     )
 )
