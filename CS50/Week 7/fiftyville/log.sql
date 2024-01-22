0ce16fde44096a7523d6836d18912b829a8749e0
jmr551 <joserivas12345@gmail.com>
2024-01-22 22:30:41 +0000

    Mon, Jan 22, 2024, 11:30 PM +01:00

--- CS50/Week 7/fiftyville/log.sql
+++ CS50/Week 7/fiftyville/log.sql
@@ -42,14 +42,15 @@ AND license_plate NOT IN (
 SELECT *
 FROM interviews
 WHERE day = 28 AND month = 7;
--- Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
--- If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                   |
--- I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
--- I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                               |
--- As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
--- In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
--- The thief then asked the person on the other end of the phone to purchase the flight ticket.
+-- 1. Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
+--    If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                   |
+-- 2. I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
+--    I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                               |
+-- 3. As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
+--    In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
+-- 4. The thief then asked the person on the other end of the phone to purchase the flight ticket.
 
+-- According to 1.
 SELECT license_plate
 FROM (SELECT activity, license_plate, hour, minute, month, year
 FROM bakery_security_logs
@@ -65,3 +66,25 @@ AND license_plate NOT IN (
     WHERE (activity = 'exit' AND hour < 10)
 ))
 WHERE activity = 'exit' AND hour = 10;
+
+-- So, the people with these license plates are
+SELECT *
+FROM people
+WHERE license_plate IN (
+    SELECT license_plate
+    FROM (SELECT activity, license_plate, hour, minute, month, year
+    FROM bakery_security_logs
+    WHERE month = 7 AND day = 28
+    AND license_plate NOT IN (
+        SELECT license_plate
+        FROM bakery_security_logs
+        WHERE (activity = 'entrance' AND hour > 10 )
+    )
+    AND license_plate NOT IN (
+        SELECT license_plate
+        FROM bakery_security_logs
+        WHERE (activity = 'exit' AND hour < 10)
+    ))
+    WHERE activity = 'exit' AND hour = 10
+
+);
