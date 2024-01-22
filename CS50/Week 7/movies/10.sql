6b74488aeeb677bf65e13156b2ce6b1e8c2446fa
jmr551 <joserivas12345@gmail.com>
2024-01-22 19:03:27 +0000

    Mon, Jan 22, 2024, 8:03 PM +01:00

--- CS50/Week 7/movies/10.sql
+++ CS50/Week 7/movies/10.sql
@@ -1,5 +1,11 @@
 SELECT name
 FROM people
 WHERE id IN (
-    SELECT 
+    SELECT person_id
+    FROM directors
+    WHERE movie_id IN (
+        SELECT id
+        FROM movies
+        WHERE 
+    )
 )
