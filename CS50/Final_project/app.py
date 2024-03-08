155c958578911287f192a87e4c0840c283dd0e7e
jmr551 <joserivas12345@gmail.com>
2024-03-08 14:35:41 +0000

    Fri, Mar 8, 2024, 3:35 PM +01:00

--- CS50/Final_project/app.py
+++ CS50/Final_project/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def upload_page():
+    return render_template('upload.html')
 
-@app.route('/upload')
-def upload_image():
-    if 'imagen' in request.files:
-        file = request.files['imagen']
-        image = Image.open(file.stream)
-        
+@app.route('/upload', methods=['POST'])
+def handle_upload():
+    if 'image' in request.files:
+        file = request.files['image']
+        image = secure_filename(file.filename)
+        filepath = os.path.join("imagen", filename)
+        file.save(filepath)
+        return redirect(url_for('edit_page', image_path=filepath))
+    return 'Could not upload the image', 400
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
