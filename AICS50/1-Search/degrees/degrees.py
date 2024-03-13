446434db22c30dd00407093a3d9196ad197e4bd2
jmr551 <joserivas12345@gmail.com>
2024-03-13 15:51:19 +0000

    Wed, Mar 13, 2024, 4:51 PM +01:00

--- AICS50/1-Search/degrees/degrees.py
+++ AICS50/1-Search/degrees/degrees.py
@@ -96,12 +96,13 @@ def shortest_path(source, target):
     queue = QueueFrontier()
     source_node = Node(source, None, None)
     for neighbor in neighbors_for_person(source):
+        print(neighbor)
         if neighbor[1]!=source:
             queue.add(Node(neighbor[1], source_node, neighbor[0]))
             print(f"neighbor = {neighbor}")
             print(f"Instruccion: queue.add(Node({neighbor[1]}, {source}, {neighbor[0]}))")
     explored = [] # Nodos explorados
-
+    '''
     while(True):
         if not queue.empty():
             nodo_actual = queue.remove()
@@ -145,7 +146,8 @@ def shortest_path(source, target):
                 explored.append(nodo_actual)
         else:
             return None
-
+    '''
+    
 def person_id_for_name(name):
     """
     Returns the IMDB id for a person's name,
