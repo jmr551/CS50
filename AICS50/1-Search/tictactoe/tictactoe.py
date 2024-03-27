cc39e2479ffcc2f1299fb083b0b74fab5d2f951a
jmr551 <joserivas12345@gmail.com>
2024-03-27 11:15:45 +0000

    Wed, Mar 27, 2024, 12:15 PM +01:00

--- AICS50/1-Search/tictactoe/tictactoe.py
+++ AICS50/1-Search/tictactoe/tictactoe.py
@@ -71,11 +71,20 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    for symbol in [X, O]:
-        # Line
-        for i in range(3):
-            if board[i][0] == board[i][1] == board[i][2]:
-                
+
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
+    return None
 
 
 def terminal(board):
