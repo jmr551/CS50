e1fdf59fd5614d29bbfb0857db91330b3263cdaf
jmr551 <joserivas12345@gmail.com>
2024-04-01 17:56:00 +0000

    Mon, Apr 1, 2024, 7:56 PM +02:00

--- Prueba/Sudoku/sudoku.py
+++ Prueba/Sudoku/sudoku.py
@@ -81,9 +81,20 @@ def solve(board):
 
     return None
 
-board = create_board()
+def create_board_1():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    return board
 
-board = play_move(board, (0, 0), 1)
-board = play_move(board, (0, 1), 1)
-print_board(board)
-print(valido(board))
+
+board = create_board_1()
+print(solve(board))
