6c91961018f76255b130aef8a45e91edfe4205df
jmr551 <joserivas12345@gmail.com>
2024-05-31 16:13:12 +0000

    Fri, May 31, 2024, 6:13 PM +02:00

---
+++ CS50P/PS1/deep/deep.py
@@ -0,0 +1,6 @@
+res = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+
+if res == "42" or res == "forty two" or res == "forty-two":
+    print ("Yes")
+else:
+    print ("No")
