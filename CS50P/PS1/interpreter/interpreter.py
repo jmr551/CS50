ecbb5f6e58f230482cc2320984bad672d3300aaa
jmr551 <joserivas12345@gmail.com>
2024-05-31 20:35:57 +0000

    Fri, May 31, 2024, 10:35 PM +02:00

---
+++ CS50P/PS1/interpreter/interpreter.py
@@ -0,0 +1,15 @@
+x, y, z = input("Expression: ").strip().split()
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    print (x + z)
+elif y == "-"
+    print (x - z)
+elif y == "*":
+    print (x * z)
+else:
+    print (x / z)
+
+
