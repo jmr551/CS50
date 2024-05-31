c66a0f4479c09364c19d4fc38883cad30fe0b157
jmr551 <joserivas12345@gmail.com>
2024-05-31 20:38:33 +0000

    Fri, May 31, 2024, 10:38 PM +02:00

--- CS50P/PS1/interpreter/interpreter.py
+++ CS50P/PS1/interpreter/interpreter.py
@@ -1,11 +1,11 @@
 x, y, z = input("Expression: ").strip().split()
 
-x = int(x)
-z = int(z)
+x = float(x)
+z = float(z)
 
 if y == "+":
-    print (x + z)
-elif y == "-"
+    print (f"{x + z}")
+elif y == "-":
     print (x - z)
 elif y == "*":
     print (x * z)
