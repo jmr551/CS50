d7f044c2e61ee3f7e41f65b8237b52e7cd7c071d
jmr551 <joserivas12345@gmail.com>
2024-05-31 18:56:04 +0000

    Fri, May 31, 2024, 8:56 PM +02:00

---
+++ CS50P/PS1/extensions/extensions.py
@@ -0,0 +1,18 @@
+file = input("File name: ").split(".")
+
+match file[-1]:
+    case "gif":
+        print("")
+    case "jpg":
+        print("")
+    case "jpeg":
+        print("")
+    case "png":
+        print("")
+    case "pdf":
+        print("")
+    case "txt":
+        print("")
+    case "zip":
+        print("")
+    d
