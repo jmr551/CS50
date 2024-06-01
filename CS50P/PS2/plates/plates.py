c496ab37c33f17348439f1be801dfe202f927850
jmr551 <joserivas12345@gmail.com>
2024-06-01 16:35:56 +0000

    Sat, Jun 1, 2024, 6:35 PM +02:00

--- CS50P/PS2/plates/plates.py
+++ CS50P/PS2/plates/plates.py
@@ -9,22 +9,27 @@ def main():
 def is_valid(s):
     # Len
     if 2 < len(s) or len(s) > 6:
+        print("Falla en len")
         return False
     # 2 chars
     if not s[0].isaplha() or not s[1].isaplha():
+        print("No cumple los dos primeros alfa")
         return False
     num_phase = False
     for c in s:
         if not c.isalnum():
+            print("No Alpha Num")
             return False
 
         if not num_phase and c.isnumeric():
             num_phase = True
             if c == "0":
+                print("Num comienza con 0")
                 return False
             continue
 
         if num_phase and c.isalpha():
+            print("Ya en num fase pero tiene un alfa luego")
             return False
     return True
 
