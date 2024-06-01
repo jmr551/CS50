6c84a5f96d7f6d004d59b10caae2ddfb5f198b40
jmr551 <joserivas12345@gmail.com>
2024-06-01 15:18:08 +0000

    Sat, Jun 1, 2024, 5:18 PM +02:00

--- CS50P/PS2/coke/coke.py
+++ CS50P/PS2/coke/coke.py
@@ -2,7 +2,8 @@ due = 50
 while due > 0:
     print("Amount Due:", due)
     coin = int(input("Insert coin: "))
-    due -= coin
+    if coin == 25 or coin == 10 or coin == 5:
+        due -= coin
 
 if due < 0:
     due *= -1
