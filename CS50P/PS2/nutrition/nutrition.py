48e6b20c169ddc16723f92808fbce8e890243aca
jmr551 <joserivas12345@gmail.com>
2024-06-01 16:57:34 +0000

    Sat, Jun 1, 2024, 6:57 PM +02:00

---
+++ CS50P/PS2/nutrition/nutrition.py
@@ -0,0 +1,26 @@
+fruits = {
+    "apple": 130,
+    "avocado California": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew Melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet Cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+    }
+
+food = input("Item: ").lower()
+if food in fruits:
+    print("Calories:", fruits[food])
