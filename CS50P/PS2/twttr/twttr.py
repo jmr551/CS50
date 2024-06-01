6ace79d0e3ff0a197f86a1b3f6e277f3ea3f1610
jmr551 <joserivas12345@gmail.com>
2024-06-01 15:27:09 +0000

    Sat, Jun 1, 2024, 5:27 PM +02:00

--- CS50P/PS2/twttr/twttr.py
+++ CS50P/PS2/twttr/twttr.py
@@ -1,4 +1,8 @@
-entrada = input("Input:")
+entrada = input("Input: ")
 wrd = ""
 for c in entrada:
-    if not c.is_vowel
+    c_min = c.lower()
+    if c_min != "a" and c_min != "e" and c_min != "i" and c_min != "o" and c_min != "u":
+        wrd += c
+
+print("Output:", wrd)
