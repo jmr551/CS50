d85351893a718900e4ef5aee118cb8e3d63ad85c
jmr551 <joserivas12345@gmail.com>
2024-06-01 16:25:23 +0000

    Sat, Jun 1, 2024, 6:25 PM +02:00

--- CS50P/PS2/plates/plates.py
+++ CS50P/PS2/plates/plates.py
@@ -10,7 +10,12 @@ def is_valid(s):
     # Len
     if 2 < len(s) or len(s) > 6:
         return False
-    
+    # 2 chars
+    if not s[0].isaplha() or not s[1].isaplha():
+        return False
+
+    for c in s:
+        c.isalnum()
 
 
 main()
