192e289a339bd5a340a0cf3457dc79edfff75e7e
jmr551 <joserivas12345@gmail.com>
2024-06-06 17:00:19 +0000

    Thu, Jun 6, 2024, 7:00 PM +02:00

--- CS50P/PS3/grocery/grocery.py
+++ CS50P/PS3/grocery/grocery.py
@@ -1,7 +1,15 @@
+def imprimir(items):
+    for item in items:
+        print(f"{item}: {items[item]}")
 items = {}
 while True:
     try:
-        items[input("").upper()} += 1
-    except :
+        item = input("").upper()
+        items[item] += 1
+    except EOFError:
+        imprimir(items)
+        break
+    except KeyError:
+        items[item] = 1
 
 
