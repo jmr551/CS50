40b4a636e879d54c5be38dd62bb353c1617e963c
jmr551 <joserivas12345@gmail.com>
2024-06-15 23:39:28 +0000

    Sun, Jun 16, 2024, 1:39 AM +02:00

---
+++ CS50P/PS5/fuel/fuel.py
@@ -0,0 +1,24 @@
+def main():
+    while True:
+        try:
+            x, y = input("Fraction: ").split("/")
+            x = int(x)
+            y = int(y)
+            f = round(x / y * 100)
+        except ValueError:
+            continue
+        except ZeroDivisionError:
+            continue
+        else:
+            if x > y:
+                continue
+            if f <= 1:
+                print("E")
+            elif f >= 99:
+                print("F")
+            else:
+                print(f"{f}%")
+            break
+
+main()
+
