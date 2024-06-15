29970f60c556fc463fc81dc5a3af99ca2af1a099
jmr551 <joserivas12345@gmail.com>
2024-06-15 15:04:50 +0000

    Sat, Jun 15, 2024, 5:04 PM +02:00

--- CS50P/PS4/bitcoin/bitcoin.py
+++ CS50P/PS4/bitcoin/bitcoin.py
@@ -6,7 +6,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) > 2:
     sys.exit("Too many arguments")
 try:
-    money = float(sys.argv[1])
+    n = float(sys.argv[1])
 except ValueError:
     sys.exit("Command-line argument is not a number")
 
