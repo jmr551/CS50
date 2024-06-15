93c8434e897f790cb174442d44cc820990d36897
jmr551 <joserivas12345@gmail.com>
2024-06-15 23:23:56 +0000

    Sun, Jun 16, 2024, 1:23 AM +02:00

--- CS50P/PS5/plates/test_plates.py
+++ CS50P/PS5/plates/test_plates.py
@@ -2,19 +2,19 @@ from plates import is_valid
 
 
 def test_CS50():
-    assert is_valid("CS50") == "Valid"
+    assert is_valid("CS50") == True
 
 def test_CS05():
-    assert is_valid("CS05") == "Invalid"
+    assert is_valid("CS05") == False
 
 def test_CS50P():
-    assert is_valid("CS50P") == "Invalid"
+    assert is_valid("CS50P") == False
 
 def test_PI314():
-    assert is_valid("PI3.14") == "Invalid"
+    assert is_valid("PI3.14") == False
 
 def test_H():
-    assert is_valid("H") == "Invalid"
+    assert is_valid("H") == False
 
 def test_OUTATIME():
-    assert is_valid("OUTATIME") == "Invalid"
+    assert is_valid("OUTATIME") == False
