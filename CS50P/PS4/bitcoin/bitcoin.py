4b6f35c21ff057ec88050e2889cfe480d8f8a4ee
jmr551 <joserivas12345@gmail.com>
2024-06-15 15:12:56 +0000

    Sat, Jun 15, 2024, 5:12 PM +02:00

--- CS50P/PS4/bitcoin/bitcoin.py
+++ CS50P/PS4/bitcoin/bitcoin.py
@@ -13,8 +13,8 @@ except ValueError:
 
 
 try:
-    respuesta = json.load(requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").content)
+    respuesta = json.loads(requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").content)
 except requests.RequestException:
     sys.exit("Error al realizar el request.")
 else:
-    print(respuesta)
+    print(respuesta.keys())
