d47991d0d82e935ea430f3e379a40b333cda7458
jmr551 <joserivas12345@gmail.com>
2024-06-15 22:35:58 +0000

    Sun, Jun 16, 2024, 12:35 AM +02:00

--- CS50P/PS5/bank/bank.py
+++ CS50P/PS5/bank/bank.py
@@ -1,10 +1,10 @@
 def main():
-    gr = input("Greeting: ").strip().lower()
+    gr = input("Greeting: ")
     print (value(gr))
 
 
 def value(gr):
-
+    gr = gr.strip().lower()
     if gr[:5] == "hello":
         return ("$0")
     elif gr[0] == 'h':
