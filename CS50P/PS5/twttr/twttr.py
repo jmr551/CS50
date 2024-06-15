b8af3cff7773c98ed7293df4f02dc8c6070cc71d
jmr551 <joserivas12345@gmail.com>
2024-06-15 20:20:13 +0000

    Sat, Jun 15, 2024, 10:20 PM +02:00

--- CS50P/PS5/twttr/twttr.py
+++ CS50P/PS5/twttr/twttr.py
@@ -1,13 +1,18 @@
 import re
+import sys
+
 def main():
     print(shorten(input("")))
 
 
 def shorten(word):
     if re.search(r'[0-9]', word):
-        raise ValueError("El texto no debe contener números")
+        print("El texto no debe contener números")
+        sys.exit(1)
+
     if re.search(r'[^\w\s]', word):
-        raise ValueError("El texto no debe contener puntuación")
+        print("El texto no debe contener puntuación")
+        sys.exit(1)
 
     z = "aeiouAEIOU"
     tabla = str.maketrans("", "", z)
