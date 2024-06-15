7c97a04a683a6f05cb9a5c63519effc76ac15bb4
jmr551 <joserivas12345@gmail.com>
2024-06-15 13:08:33 +0000

    Sat, Jun 15, 2024, 3:08 PM +02:00

--- CS50P/PS4/adieu/adieu.py
+++ CS50P/PS4/adieu/adieu.py
@@ -6,7 +6,7 @@ def main():
     while True:
         try:
             name = input("Name: ")
-        except EOFError:
+        except (EOFError, KeyboardInterrupt):
             sys.exit()
         else:
             if name == "":
@@ -20,8 +20,9 @@ def printNames(names):
     goodbye = "Adieu, adieu, to " + names[0]
 
     for i in range(1, len(names)-1):
-        goodbye += (names[i] +", to")
-    
+        goodbye += (", " + names[i])
+    if len(names) > 1:
+        goodbye += ", and " + names[-1]
     print(goodbye)
 
 if __name__ == "__main__":
