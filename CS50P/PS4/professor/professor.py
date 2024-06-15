41ce97ebb514f9ed931b3bdc708a089129257813
jmr551 <joserivas12345@gmail.com>
2024-06-15 14:08:30 +0000

    Sat, Jun 15, 2024, 4:08 PM +02:00

--- CS50P/PS4/professor/professor.py
+++ CS50P/PS4/professor/professor.py
@@ -3,16 +3,21 @@ import random
 
 def main():
     lev = get_level()
-
+    print(lev)
 
 def get_level():
     while True:
         try:
-            
+            lev = int(input("Level: "))
+        except ValueError:
+            continue
+        else:
+            if lev in [1, 2, 3]:
+                return lev
 
 
 def generate_integer(level):
-    ...
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
