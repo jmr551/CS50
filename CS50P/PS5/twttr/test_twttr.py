44a314e1f1abbbedb2059d17c58d7af7a8c3ea8a
jmr551 <joserivas12345@gmail.com>
2024-06-15 20:03:47 +0000

    Sat, Jun 15, 2024, 10:03 PM +02:00

---
+++ CS50P/PS5/twttr/test_twttr.py
@@ -0,0 +1,11 @@
+from twttr import shorten
+
+
+def minusculas():
+    assert shorten("aeiou") == ""
+    assert shorten("murcielago") == "mrclg"
+
+
+def mayusculas():
+    assert shorten("AEIOU") == ""
+    assert shorten("MURCIELAGO") == "MRCLG"
