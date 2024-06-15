c798ca6e6cc5ecf5cfff57a898f0ed8dcf0d696d
jmr551 <joserivas12345@gmail.com>
2024-06-15 14:58:50 +0000

    Sat, Jun 15, 2024, 4:58 PM +02:00

---
+++ CS50P/PS4/bitcoin/bitcoin.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+if len(sys.argv) == 1:
+    sys.exit("Missing command-line argument")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+try:
+    money = 
+
+try:
+    ...
+except requests.RequestException:
+    ...
