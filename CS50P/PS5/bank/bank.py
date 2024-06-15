aaefd79811f609bcaa93fd4150925791d1cf47dd
jmr551 <joserivas12345@gmail.com>
2024-06-15 23:02:21 +0000

    Sun, Jun 16, 2024, 1:02 AM +02:00

--- CS50P/PS5/bank/bank.py
+++ CS50P/PS5/bank/bank.py
@@ -1,21 +1,15 @@
 def main():
-    try:
-        gr = input("Greeting: ")
-        print(value(gr))
-    except Exception as e:
-        print(e)
-        sys.exit(1)
-
+    gr = input("Greeting: ")
+    print(value(gr))
 
 def value(gr):
     gr = gr.strip().lower()
     if "hello" in gr:
-        return ("$0")
-    elif gr[0] == 'h':
-        return ("$20")
+        return "$0"
+    elif gr.startswith('h'):
+        return "$20"
     else:
-        return ("$100")
-
+        return "$100"
 
 if __name__ == "__main__":
     main()
