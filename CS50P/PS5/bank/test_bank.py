0de0ad0ae71d50a304bd94cc359a62cccd6c8369
jmr551 <joserivas12345@gmail.com>
2024-06-15 22:58:37 +0000

    Sun, Jun 16, 2024, 12:58 AM +02:00

--- CS50P/PS5/bank/test_bank.py
+++ CS50P/PS5/bank/test_bank.py
@@ -1,17 +1,24 @@
 from bank import value
 
-
 def test_hello():
+    print("Test Hello")
     assert value("Hello, darling") == "$0"
 
-
 def test_hi():
+    print("Test Hi")
     assert value("Hi, darling") == "$20"
 
-
-def test_hiya():
+def test_hey():
+    print("Test Hey")
     assert value("Hey, there") == "$20"
 
-
 def test_not_h():
+    print("Test Not H")
     assert value("I'm your father") == "$100"
+
+# Ejecutar las pruebas
+if __name__ == "__main__":
+    test_hello()
+    test_hi()
+    test_hey()
+    test_not_h()
