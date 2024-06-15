93f0d483db6db61be3495c15396939ce025dfbae
jmr551 <joserivas12345@gmail.com>
2024-06-15 12:52:07 +0000

    Sat, Jun 15, 2024, 2:52 PM +02:00

--- CS50P/PS4/adieu/adieu.py
+++ CS50P/PS4/adieu/adieu.py
@@ -1,6 +1,20 @@
 import sys
-while True:
-    try:
-        input("Name")
-    except EOFError:
-        sys.exit()
+names = []
+
+def main():
+    while True:
+        try:
+            name = input("Name")
+        except EOFError:
+            sys.exit()
+        else:
+            if name == "":
+                printNames(names)
+                names = []
+            else:
+                names.append(name)
+
+
+def printNames(names):
+    goodbye = ""
+    
