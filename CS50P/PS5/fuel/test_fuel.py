0d142bd445b55fb025a22c676349d93138a342e2
jmr551 <joserivas12345@gmail.com>
2024-06-16 20:08:42 +0000

    Sun, Jun 16, 2024, 10:08 PM +02:00

--- CS50P/PS5/fuel/test_fuel.py
+++ CS50P/PS5/fuel/test_fuel.py
@@ -6,12 +6,16 @@ def test_convert_well():
     assert convert("0/1") == 0
     assert convert("1/1") == 100
     assert convert("1/2") == 50
+    with pytest.raises(ZeroDivisionError):
+        convert("2/0")
 
 def test_convert_wrong():
     with pytest.raises(ValueError):
         convert("2/1")
 
 def test_gauge():
+    assert gauge(99) == "F"
     assert gauge(100) == "F"
+    assert gauge(1) == "E"
     assert gauge(0) == "E"
     assert gauge(30) == "30%"
