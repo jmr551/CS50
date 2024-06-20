a25cccbceabec16635463ec3691d14b680ede02b
jmr551 <joserivas12345@gmail.com>
2024-06-20 14:40:37 +0000

    Thu, Jun 20, 2024, 4:40 PM +02:00

--- CS50P/PS6/pizza/pizza.py
+++ CS50P/PS6/pizza/pizza.py
@@ -5,9 +5,10 @@ if len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
-elif len(sys.argv[1]) < 4 or not (sys.argv[1][-4:] == ".csv")
+elif len(sys.argv[1]) < 4 or not (sys.argv[1][-4:] == ".csv"):
     sys.exit("Not a CSV file")
 
 with open(sys.argv[1]) as file:
     reader = csv.DictReader(file)
-    print(reader)
+    for row in reader:
+        print(row)
