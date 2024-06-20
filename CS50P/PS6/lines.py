5fb00b779f23713b6d2a53b9efad3efc5b27a538
jmr551 <joserivas12345@gmail.com>
2024-06-20 10:51:18 +0000

    Thu, Jun 20, 2024, 12:51 PM +02:00

--- CS50P/PS6/lines.py
+++ CS50P/PS6/lines.py
@@ -2,9 +2,8 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
-
-if len(sys.argv[1][-3:]) < 3 or not(sys.argv[1][-3:] == ".py"):
+elif len(sys.argv[1][-3:]) < 3 or not(sys.argv[1][-3:] == ".py"):
     sys.exit("Not a Python file")
-if len(sys.argv) > 2:
+elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
