d69a452a5d623f7cc0e45bea009b5b88374f69b9
jmr551 <joserivas12345@gmail.com>
2024-06-20 12:25:17 +0000

    Thu, Jun 20, 2024, 2:25 PM +02:00

--- CS50P/PS6/pizza/pizza.py
+++ CS50P/PS6/pizza/pizza.py
@@ -2,4 +2,8 @@ import csv
 import sys
 
 if len(sys.argv) == 1:
-    sys.exit("Too few arguments")
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv[1]) < 4 or not (sys.argv[1][-4:] == ".csv")
+    sys.exit("Not a CSV file")
