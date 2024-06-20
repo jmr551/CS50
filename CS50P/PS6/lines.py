312b1035ef3fec97f02b8b49b83bbc4bc70d8c0b
jmr551 <joserivas12345@gmail.com>
2024-06-20 11:12:56 +0000

    Thu, Jun 20, 2024, 1:12 PM +02:00

--- CS50P/PS6/lines.py
+++ CS50P/PS6/lines.py
@@ -10,11 +10,8 @@ elif len(sys.argv) > 2:
 count = 0
 with open(sys.argv[1]) as file:
     for line in file:
+        line=line.strip()
         if len(line) > 0 and line[0] != "#":
             count += 1
-
-        if count == 3:
-            print("linea:", line)
-            print(len(line))
 
 print(count)
