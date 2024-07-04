204fea94a6a70a644881f54efa216fa28f3ea936
jmr551 <joserivas12345@gmail.com>
2024-07-04 20:43:28 +0000

    Thu, Jul 4, 2024, 10:43 PM +02:00

--- CS50P/PS6/shirt/shirt.py
+++ CS50P/PS6/shirt/shirt.py
@@ -7,10 +7,14 @@ if len(sys.argv) < 3:
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 else:
-    if not (len(sys.argv[1]) > 3 and len(sys.argv[2]) > 3 and sys.argv[1][-4:] in [".jpg", ".jpeg", ".pgn"] and sys.argv[1][-4:] == sys.argv[2][-4:]):
-        sys.exit("Input and output have different extensions")
+    #if not (len(sys.argv[1]) > 3 and len(sys.argv[2]) > 3 and sys.argv[1][-4:] in [".jpg", ".jpeg", ".pgn"] and sys.argv[1][-4:] == sys.argv[2][-4:]):
+    #    sys.exit("Input and output have different extensions")
 
-        Invalid output
+    #    Invalid output
+    if sys.argv[1][-4:] not in [".jpg", ".jpeg", ".pgn"]:
+        sys.exit("Invalid input")
+    elif sys.argv[2][-4:] not in [".jpg", ".jpeg", ".pgn"]:
+        sys.exit("Invalid output")
     else:
         try:
             with Image.open(sys.argv[1]) as im:
