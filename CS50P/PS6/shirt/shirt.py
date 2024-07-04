fb46523139f36108fd2d16949c6eb202e6f1da76
jmr551 <joserivas12345@gmail.com>
2024-07-04 19:56:15 +0000

    Thu, Jul 4, 2024, 9:56 PM +02:00

--- CS50P/PS6/shirt/shirt.py
+++ CS50P/PS6/shirt/shirt.py
@@ -1,6 +1,18 @@
+from PIL import Image
 import sys
 
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
+else:
+    if not (len(sys.argv[1]) > 3 and len(sys.argv[2]) > 3 and sys.argv[1][-4:] in [".jpg", ".jpeg", ".pgn"] and sys.argv[1][-4:] == sys.argv[2][-4:]):
+        sys.exit("Input and output have different extensions")
+    else:
+        try:
+            with Image.open(sys.argv[1]) as im:
+                im.show()
+        except:
+            sys.exit("The specified input does not exist")
+
+
