d8c314744e268750579e0f0a451cfcdb1f0fcb9a
jmr551 <joserivas12345@gmail.com>
2024-07-06 18:55:17 +0000

    Sat, Jul 6, 2024, 8:55 PM +02:00

--- CS50P/PS7/watch/watch.py
+++ CS50P/PS7/watch/watch.py
@@ -7,7 +7,8 @@ def main():
 
 
 def parse(s):
-    if matches := re.match(r"\<iframe.*?src=\"https?://(:?www\.)?youtube\.com/embed/(.+?)\".*?></iframe>", s):
+    if matches := re.match(r"\<iframe.*?src=\"https?://(?:www\.)?youtube\.com/embed/(.+?)\".*?></iframe>", s):
+        print(matches.group(1))
         return True
     else:
         return False
