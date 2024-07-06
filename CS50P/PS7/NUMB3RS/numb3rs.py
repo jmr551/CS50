8b15ed663ac9637182e1e1844bdcd403fb7448c2
jmr551 <joserivas12345@gmail.com>
2024-07-06 17:53:51 +0000

    Sat, Jul 6, 2024, 7:53 PM +02:00

--- CS50P/PS7/NUMB3RS/numb3rs.py
+++ CS50P/PS7/NUMB3RS/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    matches = re.search("$d{1,3}\.d{1,3}\.d{1,3}\.d{1,3}^", ip)
+    matches = re.search(r"$d{1,3}\.d{1,3}\.d{1,3}\.d{1,3}^", ip)
     print(matches.group(1))
     print(matches.group(2))
     print(matches.group(3))
