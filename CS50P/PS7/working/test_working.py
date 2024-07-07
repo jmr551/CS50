91f317232fc5917c128e3430b742592348fd77da
jmr551 <joserivas12345@gmail.com>
2024-07-07 17:53:11 +0000

    Sun, Jul 7, 2024, 7:53 PM +02:00

--- CS50P/PS7/working/test_working.py
+++ CS50P/PS7/working/test_working.py
@@ -3,8 +3,10 @@ from working import convert
 
 def test_right():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
-    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
-    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5:30 PM") == "09:00 to 17:30"
+    assert convert("9:30 AM to 5 PM") == "09:30 to 17:00"
+
 
 def test_wrong():
     with pytest.raises(ValueError):
