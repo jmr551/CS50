6c70eb0247c4679e040640ba3a8bec6300e09464
jmr551 <joserivas12345@gmail.com>
2024-07-07 18:54:24 +0000

    Sun, Jul 7, 2024, 8:54 PM +02:00

--- CS50P/PS7/response/response.py
+++ CS50P/PS7/response/response.py
@@ -1,7 +1,14 @@
 import re
 
 def main():
-    
+    print(valid(input("What's your email address? ")))
+
+def valid(ad):
+    m = r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+    if re.search(m, ad):
+        return "Valid"
+    else:
+        return "Invalid"
 
 if __name__ == "__main__":
     main()
