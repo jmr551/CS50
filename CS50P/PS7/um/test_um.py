8d9b4232e4fd99cb2d4c44ea960da9ff0786c3c8
jmr551 <joserivas12345@gmail.com>
2024-07-07 18:31:05 +0000

    Sun, Jul 7, 2024, 8:31 PM +02:00

---
+++ CS50P/PS7/um/test_um.py
@@ -0,0 +1,8 @@
+from um import count
+
+def test_count():
+    assert count("um, hello, um, world") ==
+    assert count("um...") ==
+    assert count("yum") ==
+    assert count("yummy") ==
+    assert count("um aja um umm sum um") ==
