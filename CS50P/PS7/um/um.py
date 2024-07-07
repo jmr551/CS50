0f233bc3aa104e7b49a411f2d80ba60505bfe57d
jmr551 <joserivas12345@gmail.com>
2024-07-07 18:26:39 +0000

    Sun, Jul 7, 2024, 8:26 PM +02:00

--- CS50P/PS7/um/um.py
+++ CS50P/PS7/um/um.py
@@ -8,7 +8,7 @@ def main():
 
 def count(s):
     matches = re.findall(r"(^um[^a-z]|[^a-z]um[^a-z]|[^a-z]um$|^um$)", s, flags=re.IGNORECASE)
-    return (matches)
+    return len(matches)
 
 
 if __name__ == "__main__":
